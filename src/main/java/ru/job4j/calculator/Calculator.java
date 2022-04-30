1920b268b647fa0a64916e1609cdb7b0a35403eb
zenit1925 <kostia-85-85@yandex.ru>
2022-04-30 11:58:07 +0300

    4.2. Результат работы метода [#232622] - вернул метод

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
         int six = 6;
         int four = 4;
         int five = 5;
