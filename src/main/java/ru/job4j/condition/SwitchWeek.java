b113afa8425bae66420441b19b7126186d6eeffc
zenit1925 <kostia-85-85@yandex.ru>
2022-05-05 09:11:44 +0300

    4.6.1. Switch [#334070 #297139] - Исправление

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -1,24 +1,32 @@
 package ru.job4j.condition;
 
 public class SwitchWeek {
-
     public static String nameOfDay(int day) {
-        String name = switch (day) {
-            case 1 -> "Monday";
-            case 2 -> "Tuesday";
-            case 3 -> "Wednesday";
-            case 4 -> "Thursday";
-            case 5 -> "Friday";
-            case 6 -> "Saturday";
-            case 7 -> "Sunday";
-            default -> "Error";
-        };
-
+        String name;
+        switch (day) {
+            case 1:
+                name = "Monday";
+                break;
+            case 2:
+                name = "Tuesday";
+                break;
+            case 3:
+                name = "Wednesday";
+                break;
+            case 4:
+                name = "Thursday";
+                break;
+            case 5:
+                name = "Friday";
+                break;
+            case 6:
+                name = "Saturday";
+                break;
+            case 7:
+                name = "Sunday";
+            default:
+                name = "Error";
+        }
         return name;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(nameOfDay(8));
-        System.out.println(nameOfDay(5));
     }
 }
