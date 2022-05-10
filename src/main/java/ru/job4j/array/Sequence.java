bf06cbd381f0d25a6c844bc6acdb92e8f47a5de7
zenit1925 <kostia-85-85@yandex.ru>
2022-05-10 10:55:45 +0300

    6.6.1. Двумерный массив [#226796]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] arrayFirst = new int[3][3];
+        arrayFirst[0][0] = 1;
+        arrayFirst[0][1] = 2;
+        arrayFirst[0][2] = 3;
+
+        arrayFirst[1][0] = 1;
+        arrayFirst[1][1] = 2;
+        arrayFirst[1][2] = 3;
+
+        arrayFirst[2][0] = 1;
+        arrayFirst[2][1] = 2;
+        arrayFirst[2][2] = 3;
+    }
+}
