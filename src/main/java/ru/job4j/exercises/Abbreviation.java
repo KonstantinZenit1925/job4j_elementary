5c9fb9b6faad891b7637db7c4400a38b6fb97b46
zenit1925 <kostia-85-85@yandex.ru>
2022-05-15 11:39:16 +0300

    Упражнения, корректировка комментариев.

--- src/main/java/ru/job4j/exercises/Abbreviation.java
+++ src/main/java/ru/job4j/exercises/Abbreviation.java
@@ -1,14 +1,17 @@
 package ru.job4j.exercises;
 
+/**
+ * Создаем объект класса стрингбилдер
+ * в цикле пробегаем по строкам стринга,через сплит определяем пробелы
+ * склеиваем возвращаемое значение 0 и 1 индекса, преобразуем все знаки в данной строке в верхний регистр
+ */
+
 public class Abbreviation {
     public static String collect(String s) {
-        StringBuilder sb1 = new StringBuilder();  // создаем объект класса стрингбилдер
-
-        for (String i : s.split(" ")) {       // в цикле пробегаем по строкам стринга,
-                                                   // через сплит определяем пробелы
-
-            sb1.append(i.substring(0, 1).toUpperCase());  // склеиваем возвращаемое значение 0 и 1 индекса
-        }                                             // преобразуем все знаки в данной строке в верхний регистр
+        StringBuilder sb1 = new StringBuilder();
+        for (String i : s.split(" ")) {
+            sb1.append(i.substring(0, 1).toUpperCase());
+        }
         return sb1.toString();
     }
 
