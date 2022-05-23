013525a3432ac0332930291bc44c40eaee82cd5f
zenit1925 <kostia-85-85@yandex.ru>
2022-05-23 15:05:39 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,45 +8,37 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when100150to200300then180dot27() {
-        double expected = 180.27;
-        int x1 = 100;
-        int y1 = 150;
-        int x2 = 200;
-        int y2 = 300;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when100to00then10() {
+        Point a = new Point(10, 0);
+        Point b = new Point(0, 0);
+        double expected = 10;
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when2550to7510then64dot03() {
-        double expected = 64.03;
-        int x1 = 25;
-        int y1 = 50;
-        int x2 = 75;
-        int y2 = 10;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when05to00then10() {
+        Point a = new Point(0, 5);
+        Point b = new Point(0, 0);
+        double expected = 5;
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when101000to10000100000then99502dot76() {
-        double expected = 99502.76;
-        int x1 = 10;
-        int y1 = 1000;
-        int x2 = 10000;
-        int y2 = 100000;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when015to50then15dot81() {
+        Point a = new Point(0, 15);
+        Point b = new Point(5, 0);
+        double expected = 15.81;
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
